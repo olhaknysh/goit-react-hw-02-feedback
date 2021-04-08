be41d6a550a00fac46c8f2da5b6595c357fda430
olhaknysh <olhaknysh1994@gmail.com>
2021-04-08 18:01:54 +0300

    Updates

---
+++ static/js/main.bcaa0e89.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-02-feedback"]=this["webpackJsonpgoit-react-hw-02-feedback"]||[]).push([[0],{9:function(e,t,c){"use strict";c.r(t);var n=c(1),r=c.n(n),a=c(3),i=c.n(a),o=c(0);var s=function(){return Object(o.jsx)("div",{})};i.a.render(Object(o.jsx)(r.a.StrictMode,{children:Object(o.jsx)(s,{})}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.bcaa0e89.chunk.js.map
